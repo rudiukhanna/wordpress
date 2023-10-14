acc1ef69a5d937c85e98694b7e3c7edb0835c1ee
Hanna <melomanna@gmail.com>
2023-10-14 22:41:30 +0300

    burger menu was animated with js

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    /*smooth menu scrolling */
+    $('nav a[href^="#"]').click(function() {
+        let target = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 500);
+        $('nav a[href^="#"]').parent().removeClass('active');
+        $(this).parent().addClass('active');
+        $('.menu').toggle(500);
+        $('.menu__burger').toggleClass('close');
+        return false;
+    });
+
+    /* mobile menu */
+    $('.menu__burger').click(function() {
+        $('.menu').toggle(500);
+        $(this).toggleClass('close');
+    });
+});
